48c525477f2ae9012d6e3b573a2ef3614d11c6c6
isaac.carroll <isaac.carroll@77a93365-1838-0410-bd9a-b1cab423ab52>
2007-09-19 02:55:22 +0000

    rename source directory to code make universal moon.py startup script add svn:ignore to code directory

--- trunk/code/settings.py
+++ trunk/code/settings.py
@@ -16,7 +16,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 """
 import pygame
-import moon
 
 def main_settings(game):
     #this is the 'main' settings menu
