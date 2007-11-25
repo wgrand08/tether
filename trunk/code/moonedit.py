7dbd21b4ebaee13f27a4670d58baa5e3c79bdd6b
donkyhotay@verizon.net <donkyhotay@verizon.net@77a93365-1838-0410-bd9a-b1cab423ab52>
2007-11-25 04:26:25 +0000

    renamed editor to avoid confusion

--- trunk/code/moonedit.py
+++ trunk/code/moonedit.py
@@ -16,6 +16,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 """
 
-def editor_screen(game)
+def editor_screen(game):
     print("editor screen placeholder")
     
