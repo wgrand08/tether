2cedff81c18bc4885add641d7cf65fd8ffb632bf
donkyhotay@verizon.net <donkyhotay@verizon.net@77a93365-1838-0410-bd9a-b1cab423ab52>
2008-03-24 23:18:47 +0000

    system transfer

--- trunk/code/host.py
+++ trunk/code/host.py
@@ -22,13 +22,32 @@ import hostgvars
 import socket
 import threading
 import pickle
+import Queue
 
 
 #useful example located at:
 # http://www.devshed.com/c/a/Python/Basic-Threading-in-Python/1/
 class moonHost(threading.Thread):
-	def __init__ ( self, channel, details ):
-		threading.Thread.__init__ ( self )
+	#def __init__ ( self, channel, details ):
+		#threading.Thread.__init__ ( self )
 
-	def runhost(self):
-		print"placeholder"
+	def run(self):
+		print"start runhost"
+		while True:
+		client = clientPool.get()
+		if client != None:
+		print"Recieved connection: ", client [1] [0]
+		for x in xrange (10):
+			print client [0].recv (1024)
+		client [0].close()
+		print"Closed connection: ", client [1] [0]
+
+clientPool = Queue.Queue (0)
+for x in xrange(2):
+	moonHost().start()
+server = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+server.bind (('', 2727))
+server.listen (5)
+while True:
+	channel, details = server.accept
+	moonHost (channel, details ).start()
--- trunk/code/hostgvars.py
+++ trunk/code/hostgvars.py
@@ -19,3 +19,4 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 clock = 1
 networkversion = 0
+runhost = False
