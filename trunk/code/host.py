94d8f44820df6fc6d410311e9068144faf5185a7
donkyhotay@verizon.net <donkyhotay@verizon.net@77a93365-1838-0410-bd9a-b1cab423ab52>
2008-04-07 02:24:34 +0000

    added threaded server code

--- trunk/code/host.py
+++ trunk/code/host.py
@@ -21,22 +21,76 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 # example code located: http://www.devshed.com/c/a/Python/Sockets-in-Python-Into-the-World-of-Python-Network-Programming/2/ & http://ilab.cs.byu.edu/python/socketmodule.html
 
+import select
 import socket
+import sys
+import threading 
 
 
-def starthost():	host = ''
-	port = 50000
-	backlog = 5
-	size = 1024
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((host,port))
-	s.listen(backlog)
-	while 1:
-		client, address = s.accept()
-		data = client.recv(size)
-		if data:
-			client.send(data)
-			print"sent: ", data
-		client.close()
+class Server:
+    def __init__(self):
+        self.host = ''
+        self.port = 50000
+        self.backlog = 5
+        self.size = 1024
+        self.server = None
+        self.threads = []
 
-starthost()
+    def open_socket(self):
+        try:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind((self.host,self.port))
+            self.server.listen(5)
+        except socket.error, (value,message):
+            if self.server:
+                self.server.close()
+            print "Could not open socket: " + message
+            sys.exit(1)
+
+    def run(self):
+        self.open_socket()
+        input = [self.server,sys.stdin]
+        running = 1
+        while running:
+            inputready,outputready,exceptready = select.select(input,[],[])
+
+            for s in inputready:
+
+                if s == self.server:
+                    # handle the server socket
+                    c = Client(self.server.accept())
+                    c.start()
+                    self.threads.append(c)
+
+                elif s == sys.stdin:
+                    # handle standard input
+                    junk = sys.stdin.readline()
+                    running = 0
+
+        # close all threads
+
+        self.server.close()
+        for c in self.threads:
+            c.join()
+
+class Client(threading.Thread):
+    def __init__(self,(client,address)):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.address = address
+        self.size = 1024
+
+    def run(self):
+        running = True
+        while running:
+            data = self.client.recv(self.size)
+            if data:
+                self.client.send(data)
+		print"sent: ", data
+            else:
+                self.client.close()
+                running = False
+
+if __name__ == "__main__":
+    s = Server()
+    s.run()
