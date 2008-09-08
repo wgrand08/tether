bb91d06461418b2c6c93b95f599d66605c812b09
donkyhotay@verizon.net <donkyhotay@verizon.net@77a93365-1838-0410-bd9a-b1cab423ab52>
2008-09-08 20:19:49 +0000

    disabled extra units

--- trunk/server/gameserverstate.py
+++ trunk/server/gameserverstate.py
@@ -50,10 +50,10 @@ class ServerState:
       #FIXME: These are just for tests.
 
       self.game.create_unit('hub', (20,22));
-      self.game.create_unit('hub', (25,25));
-      self.game.create_unit('hub', (40,12));
-      self.game.create_unit('hub', (70,80));
-      self.game.create_unit('hub', (64,82));
+      #self.game.create_unit('hub', (25,25));
+      #self.game.create_unit('hub', (40,12));
+      #self.game.create_unit('hub', (70,80));
+      #self.game.create_unit('hub', (64,82));
       self.ooga = 0;
 
       #Initialize main loop callback.
