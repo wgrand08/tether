1574ea60c26c18964de86422ab6d3e4e72686769
donkyhotay@verizon.net <donkyhotay@verizon.net@77a93365-1838-0410-bd9a-b1cab423ab52>
2008-09-28 23:55:33 +0000

    only allow users to select buildings

--- trunk/client/mapctrl.py
+++ trunk/client/mapctrl.py
@@ -138,11 +138,12 @@ class Mapctrl:
                 unit = self.client.map.get_unit(map_pos);
                 if unit: #todo: add code to prevent users from selecting enemy units
                     self.client.selected_unit = {};
-                    if len(self.client.selected_unit.values()) == 0: #this is to prevent user from selecting multiple units
-                        self.client.selected_unit.update({map_pos:unit});
-                        #self.set_mouse_state('goto'); #enable to test unit movement
-                        logging.info("Selected unit ID %r" % unit.id);
-                        logging.info("Selected unit typeset %r" % unit.typeset);
+                    if unit.typeset == "build":
+                        if len(self.client.selected_unit.values()) == 0: #this is to prevent user from selecting multiple units
+                            self.client.selected_unit.update({map_pos:unit});
+                            #self.set_mouse_state('goto'); #enable to test unit movement
+                            logging.info("Selected unit ID %r" % unit.id);
+                            logging.info("Selected unit typeset %r" % unit.typeset);
                 yy += 1;
                 y += inc_y;
                 xx += 1;
