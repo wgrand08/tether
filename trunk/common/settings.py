06ebb859b6de4e374227de7dac3c3844d2d7c8ab
donkyhotay@verizon.net <donkyhotay@verizon.net@77a93365-1838-0410-bd9a-b1cab423ab52>
2008-10-06 01:30:16 +0000

    removed audacity files

--- trunk/common/settings.py
+++ trunk/common/settings.py
@@ -38,7 +38,7 @@ class GameSettings:
         self.language = "en" 
         self.psyco = "true";
 
-        self.version = 0.410;
+        self.version = 0.421;
         self.playername = "Commander";
         self.fullscreen = False;
         self.WINDOW_SIZE = self.screen_width,self.screen_height = 1024,768;
