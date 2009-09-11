89618bd2cdd21fcaf784463ba49aa81e351c9c19
donkyhotay@verizon.net <donkyhotay@verizon.net@77a93365-1838-0410-bd9a-b1cab423ab52>
2009-09-11 04:14:41 +0000

    added moonaudio to svn

---
+++ trunk/client/moonaudio.py
@@ -0,0 +1,37 @@
+"""Copyright 2009:
+    Isaac Carroll, Kevin Clement, Jon Handy, David Carroll, Daniel Carroll
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+"""
+
+
+import pygame
+import os
+import logging
+
+class MoonAudio:
+    def __init__(self, client):
+        self.client = client
+
+    def play_music(self, chosen_song):
+        if self.client.settings.play_music == True:
+            pygame.mixer.music.load(os.path.join('data/music',chosen_song))
+            pygame.mixer.music.play(-1)
+
+    def play_sound(self, chosen_sound):
+        if self.client.settings.play_sound == True:
+            sound = pygame.mixer.Sound(os.path.join('data/sounds',chosen_sound))
+            sound.play()
+
