b30a171a0ec26657e13b6095cfbdb7e9fb00cca2
donkyhotay@verizon.net <donkyhotay@verizon.net@77a93365-1838-0410-bd9a-b1cab423ab52>
2009-09-28 06:28:10 +0000

    fixed powerbar direction bug

--- trunk/common/settings.py
+++ trunk/common/settings.py
@@ -37,8 +37,8 @@ class GameSettings:
         self.screen_height = 768
         self.language = "en" 
 
-        self.version = 0.675
-        self.string_version = "0.6.75"
+        self.version = 0.676
+        self.string_version = "0.6.76"
         self.max_players = 4
         self.playername = "Commander"
         self.fullscreen = False
