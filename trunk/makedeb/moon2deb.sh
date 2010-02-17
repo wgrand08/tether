175fe87c0745f556dddb654331951c4774128813
donkyhotay@verizon.net <donkyhotay@verizon.net@77a93365-1838-0410-bd9a-b1cab423ab52>
2010-02-17 01:23:10 +0000

    added basic makedeb scripts

---
+++ trunk/makedeb/moon2deb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+#Copyright 2009:
+#    Isaac Carroll, Kevin Clement, Jon Handy, David Carroll, Daniel Carroll
+
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+
+#this script is used for automating the process of creating a 
+#debfile out of MoonPy
+
+
+cd ..
+find . -name .svn -exec rm -rf {} \;
+find . -name *.pyc -exec rm -rf {} \;
+
+gzip ./moonpy.tar.gz ./client ./common ./data ./server ./translations ./twisted ./zope ./AUTHORS.txt ./COPYING.txt ./moon.py ./README.txt
