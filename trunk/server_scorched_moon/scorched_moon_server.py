16c4dc08bb2e544a37ca8513bb4bf8c98df5e388
donkyhotae@gmail.com <donkyhotae@gmail.com@77a93365-1838-0410-bd9a-b1cab423ab52>
2014-06-09 06:10:06 +0000

    basic networking framework

---
+++ trunk/server_scorched_moon/scorched_moon_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+"""Copyright 2014:
+    Kevin Clement
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+"""
+
+import sys
+import subprocess
+import os
+
+def main():
+    debug = False
+    check = 0
+    for argument in sys.argv:
+        if argument == "--debug" or argument == "-d":
+            debug = True
+        elif argument == "--help" or argument == "-h":
+            usage()
+        elif check > 0:
+            print"Unknown argument: " + argument
+            usage()
+        check += 1
+
+    import server.main
+    server = server.main.Main(debug)
+
+def usage():
+    print"usage: [--debug] [--help]"
+    sys.exit(0)
+
+main()
---
+++ trunk/server_scorched_moon/server/main.py
@@ -0,0 +1,72 @@
+"""Copyright 2014:
+    Kevin Clement
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+"""
+
+import sys
+import select
+import os
+import logging
+from miniboa import TelnetServer
+
+#the main server class that handles everything
+
+class Main:
+
+    def __init__(self, debug):
+
+        self.debug = debug
+        self.shutdown_command = False
+        self.version = 0.001
+        self.gameIDs = []
+        self.playerIDs = []
+        self.clientlist = []
+        self.runserver = True
+
+        def process_clients():
+            for client in self.clientlist:
+                if client.active and client.cmd_ready:
+                    cmd = client.get_command()
+                    if cmd == "exit":
+                        client.active = False
+                    else:
+                        client.send("Unknown Command")
+
+        def client_connects(client):
+            print "%s connected to server" % client.address
+            self.clientlist.append(client)
+            client.send("Welcome to Scorched Moon version %s\n" % self.version)
+
+        def client_disconnects(client):
+            print "%s disconnected from server" % client.address
+            client.send("Disconnecting you from server")
+            self.clientlist.remove(client)
+
+
+        self.server = TelnetServer(port=6112, on_connect=client_connects, on_disconnect=client_disconnects)
+
+        if debug == True:
+            print "Server is running in debug mode"
+        else:
+            print "Server is not running in debug mode"
+
+
+        ## Server Loop
+        while self.runserver:
+            self.server.poll()        # Send, Recv, and look for new connections
+            process_clients()           # Check for client input
+
+        print "Server Shutdown"
